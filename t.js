2bd8be179c41807c5b112fe4354357bb53c938ea
natthp49 <natthapat.p@aware.co.th>
2024-08-16 17:15:40 +0700

    update

---
+++ t.js
@@ -0,0 +1,5 @@
+// var argv = require('minimist')(process.argv.slice(2));
+console.log("2 => ",process.argv.slice(2));
+console.log("3 => ",process.argv.slice(3));
+console.log("4 => ",process.argv.slice(4));
+console.log("5 => ",process.argv.slice(5));
